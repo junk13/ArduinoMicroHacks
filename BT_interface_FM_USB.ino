d3050cb7c39865e79b483503c1cf861eb77120a9
SinanAkkoyun <43215895+SinanAkkoyun@users.noreply.github.com>
2018-12-03 16:47:19 +0100

    Update BT_interface_FM_USB.ino

--- BT_interface_FM_USB.ino
+++ BT_interface_FM_USB.ino
@@ -1,13 +1,22 @@
 #include "Keyboard.h"
 #include "Mouse.h"
-#include "usToDE.h"
+#include <Wire.h>
+#include <Adafruit_Si4713.h>
+
+#define RESETPIN 7
+#define FMSTATION 10230      // 10230 == 102.30 MHz
+Adafruit_Si4713 radio = Adafruit_Si4713(RESETPIN);
+bool isRadioConnected = 0;
+bool isRDSon = 0;
+char* station;
+char* message;
 
 #define INPUT_SIZE 2056
 
 char ctrlKey = KEY_LEFT_GUI;
 int stdDelay = 50;
-char* help = "std:help/splash\nCMD:\nmouser:press/release, mousel:press/release,mousemov:x,y\nkeycmd:GUIr/lock/up/down/left/right, keyln:string, key:string\nstddelay:time";
-char* splash = "H4ck1ng t00l v1.0\nTo broadcast/pirate radio stations enter following cmd: radio:1003 (for 100.3)\nYou can execute multiple commands as you append them with an &.\nTo hijack USB Mouse and Keyboard on your target, type this: keycmd:GUIr&keyln:cmd&key:hacked&mouse:0 30&mouse:click\nHave fun!\n\n";
+char* help = "std:help/splash\nCMD:\nmouser:press/release, mousel:press/release,mousemov:x,y\nkeycmd:GUIr/lock/up/down/left/right, keyln:string, key:string\nstddelay:time\nradio:10030, radio:off, radiostation:stationname, radiomsg:buffer";
+char* splash = "H4ck1ng t00l v1.0\nTo broadcast/pirate radio stations enter following cmd: radio:10030 (for 100.30)\nYou can execute multiple commands as you append them with an &.\nTo hijack USB Mouse and Keyboard on your target, type this: keycmd:GUIr&keyln:cmd&key:hacked&mouse:0 30&mouse:click\nHave fun!\n\n";
 bool isSerial = 0;
 
 void setup()
@@ -18,6 +27,12 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial1.println("booted successfully");
+
+  if (!radio.begin()) {
+    Serial1.println("No radio connected...");
+  } else {
+    isRadioConnected = 1;
+  }
 }
 
 //digitalWrite(LED_BUILTIN, HIGH);
@@ -132,7 +147,43 @@ void loop() {
         }
       }
       if(strcmp(cmd,"radio") == 0) {
+        if(isRadioConnected && strcmp(separator,"off") != 0) {
+          int freq = atoi(separator);
 
+          Serial1.print("\nSet TX power");
+          radio.setTXpower(115);  // dBuV, 88-115 max
+        
+          Serial1.print("\nTuning into "); 
+          Serial1.print(freq/100); 
+          Serial1.print('.'); 
+          Serial1.println(freq % 100);
+          radio.tuneFM(freq);
+
+          radio.readTuneStatus();
+          Serial1.print("\tCurr freq: "); 
+          Serial1.println(radio.currFreq);
+          Serial1.print("\tCurr freqdBuV:"); 
+          Serial1.println(radio.currdBuV);
+          Serial1.print("\tCurr ANTcap:"); 
+          Serial1.println(radio.currAntCap);
+        } else if(strcmp(separator,"off")) {
+          radio.reset();
+          isRSDon = 0;
+        }
+      }
+      if(strcmp(cmd,"radiostation") == 0) {
+        if(!isRDSon) {
+          radio.beginRDS();
+          isRSDon = 0;
+        }
+        radio.setRDSstation(separator);
+      }
+      if(strcmp(cmd,"radiomsg") == 0) {
+        if(!isRDSon) {
+          radio.beginRDS();
+          isRSDon = 0;
+        }
+        radio.setRDSbuffer(separator);
       }
     }
     command = strtok(0, "&"); // Find the next command in input string
